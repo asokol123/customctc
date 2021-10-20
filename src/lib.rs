24d608b5baf60c1c2a0376089b4b4e292f886fea
Alexey Sokolovskiy <alexej.sokolovskiy@gmail.com>
2021-10-21 01:11:22 +0300

    Added lm model check

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,19 @@ use numpy::PyArray2;
 use ordered_float::OrderedFloat;
 use pyo3::exceptions::PyAssertionError;
 use pyo3::prelude::*;
-use pyo3::types::PyString;
+use pyo3::types::{PyFloat, PyString};
 
 #[pymodule]
 fn customctc(_py: Python<'_>, _m: &PyModule) -> PyResult<()> {
-    #[pyfn(_m)]
-    #[pyo3(name = "beam_search")]
+    #[pyfn(_m, beam_size = 100)]
+    #[pyo3(name = "beam_search", text_signature = "(probs, alphabet, beam_size = 100, lm = None")]
     fn beam_search<'py>(
         _py: Python<'py>,
         probs: &PyArray2<f32>,
         alphabet: &PyString,
         beam_size: usize,
-    ) -> PyResult<Vec<(f32, String)>> {
+        lm: Option<&PyAny>,
+    ) -> PyResult<Vec<(f32, String, u8)>> {
         let alphabet = alphabet.to_str()?.as_bytes();
         if probs.shape().len() != 2 {
             return Err(PyAssertionError::new_err(format!(
@@ -21,7 +22,6 @@ fn customctc(_py: Python<'_>, _m: &PyModule) -> PyResult<()> {
                 probs.shape().len(),
             )));
         }
-        let char_length = probs.shape()[0];
         let voc_size = probs.shape()[1];
         if voc_size != alphabet.len() {
             return Err(PyAssertionError::new_err(format!(
@@ -32,62 +32,59 @@ fn customctc(_py: Python<'_>, _m: &PyModule) -> PyResult<()> {
         }
 
         let probs = unsafe { probs.as_array() };
-        let mut hypos = probs
-            .row(0)
-            .as_slice()
-            .unwrap()
-            .iter()
-            .skip(1)
-            .enumerate()
-            .filter_map(|(i, &p)| {
-                if alphabet[i + 1] != b' ' {
-                    Some((p, String::from(alphabet[i + 1] as char)))
-                } else {
-                    None
-                }
-            })
-            .collect::<Vec<_>>();
 
-        for i in 1..char_length {
-            let mut heap = std::collections::BTreeSet::<(OrderedFloat<f32>, String)>::new();
-            let mut hypos_set = std::collections::HashMap::<String, f32>::new();
-            while !hypos.is_empty() {
-                let (prob, hypo) = hypos.pop().unwrap();
-                for (j, &p) in probs.row(i).as_slice().unwrap()[1..].iter().enumerate() {
-                    let cur_prob = prob + p;
-                    let cur_hypo = if hypo.as_bytes()[hypo.len() - 1] != alphabet[j + 1] {
-                        let mut hypo = hypo.clone();
-                        hypo.push(alphabet[j + 1] as char);
-                        hypo
-                    } else {
+        let mut hypos = vec![(1., String::new(), b'^')];
+        for i in 0..probs.shape()[0] {
+            let mut heap = std::collections::BTreeSet::<(OrderedFloat<f32>, String, u8)>::new();
+            let mut hypos_set = std::collections::HashMap::new();
+            for (prob, hypo, path) in hypos.drain(..) {
+                for (j, &p) in probs.row(i).iter().enumerate() {
+                    let cur_path = alphabet[j];
+                    let mut cur_prob = prob * p;
+                    let cur_hypo = if path == cur_path || j == 0 {
                         hypo.clone()
+                    } else {
+                        let mut hypo = hypo.clone();
+                        hypo.push(cur_path as char);
+                        hypo
                     };
 
-                    if let Some(&cur_hypo_prob) = hypos_set.get(&cur_hypo) {
-                        if cur_hypo_prob < cur_prob {
-                            heap.remove(&(OrderedFloat::from(cur_hypo_prob), cur_hypo.clone()));
-                            hypos_set.insert(cur_hypo.clone(), cur_prob);
-                            heap.insert((OrderedFloat::from(cur_prob), cur_hypo));
-                        }
+                    if let Some(lm) = lm {
+                        let lm_prob: f64 = lm
+                            .call_method1("score", (&cur_hypo,))?
+                            .downcast::<PyFloat>()?
+                            .value();
+
+                        cur_prob += (0.9 * lm_prob.exp() + 0.0001 * (i as f64)) as f32;
+                    }
+
+                    if let Some(&prev_prob) = hypos_set.get(&(cur_hypo.clone(), cur_path)) {
+                        heap.remove(&(OrderedFloat::from(prev_prob), cur_hypo.clone(), cur_path));
+                        heap.insert((
+                            OrderedFloat::from(cur_prob + prev_prob),
+                            cur_hypo.clone(),
+                            cur_path,
+                        ));
+                        hypos_set.insert((cur_hypo, cur_path), cur_prob + prev_prob);
                     } else if heap.len() < beam_size {
-                        hypos_set.insert(cur_hypo.clone(), cur_prob);
-                        heap.insert((OrderedFloat::from(cur_prob), cur_hypo));
+                        heap.insert((OrderedFloat::from(cur_prob), cur_hypo.clone(), cur_path));
+                        hypos_set.insert((cur_hypo, cur_path), cur_prob);
                     } else {
                         let first = heap.iter().next().unwrap();
-                        if first.0.into_inner() < cur_prob {
+                        let first_prob = first.0.into_inner();
+                        if first_prob < cur_prob {
                             let first = first.clone();
                             heap.remove(&first);
-                            hypos_set.remove(&first.1);
-                            hypos_set.insert(cur_hypo.clone(), cur_prob);
-                            heap.insert((OrderedFloat::from(cur_prob), cur_hypo));
+                            hypos_set.remove(&(first.1.clone(), first.2));
+                            heap.insert((OrderedFloat::from(cur_prob), cur_hypo.clone(), cur_path));
+                            hypos_set.insert((cur_hypo, cur_path), cur_prob);
                         }
                     }
                 }
             }
-
             hypos = heap
                 .into_iter()
-                .map(|(prob, hypo)| (prob.into_inner(), hypo))
+                .map(|(prob, hypo, path)| (prob.into_inner(), hypo, path))
                 .collect();
         }
 
@@ -95,12 +92,4 @@ fn customctc(_py: Python<'_>, _m: &PyModule) -> PyResult<()> {
     }
 
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
